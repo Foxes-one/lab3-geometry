ca90fb28dc4c3505f4b04e7cca5b110802d7b6bd
Потапов Роман Владимирович <ip812s25@mwp12.csc.local>
2019-05-13 11:49:36 +0700

    created input.h

---
+++ src/input.h
@@ -0,0 +1,12 @@
+#ifndef INPUT_H
+#define INPUT_H
+#include "geometry.h"
+
+int Input(Figure* new, char* A);
+int Coordinats(Figure* new, char* A);
+int Extra_sumbol(char* arr, int i);
+int PunctuationC(char* arr, int i);
+int Punctuation(char* arr);
+int First_Character(char* arr);
+
+#endif
